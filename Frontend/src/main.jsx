2a35606ecc83bb8eb13a212bc334d26979b9e074
Juvenaawh <cph-jh662@cphbusiness.dk>
2025-04-25 20:28:35 +0200

    Create new blog post for frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BlogPostFrontPage from './pages/BlogPostFrontPage.jsx'
+import CreateBlogPost from './pages/CreateBlogPost.jsx'
 //import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    {/* <App />*/}
-   <BlogPostFrontPage />
+   {/* <BlogPostFrontPage /> */}
+   <CreateBlogPost />
   </StrictMode>,
 )
