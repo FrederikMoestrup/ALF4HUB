96fae163145d3c17e01b7fe061a26be65209ae60
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-04-27 22:48:55 +0200

    Add display name to games

--- Backend/src/main/java/dat/enums/Game.java
+++ Backend/src/main/java/dat/enums/Game.java
@@ -1,5 +1,23 @@
 package dat.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Game {
-    COUNTER_STRIKE, LEAGUE_OF_LEGENDS, DOTA_2, VALORANT, ROCKET_LEAGUE
+    COUNTER_STRIKE("Counter-Strike"),
+    LEAGUE_OF_LEGENDS("League of Legends"),
+    DOTA_2("Dota 2"),
+    VALORANT("Valorant"),
+    ROCKET_LEAGUE("Rocket League");
+
+    private final String displayName;
+
+    Game(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
 }
