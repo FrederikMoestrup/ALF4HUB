91b1ca557b55d2571b38118c86c5b7d7699a077b
peterjanas <pejanas@gmail.com>
2025-04-28 07:08:29 +0200

    unittests

---
+++ Backend/src/test/java/dat/daos/TeamDAOTest.java
@@ -0,0 +1,46 @@
+package dat.daos;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamDAOTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void getById()
+    {
+    }
+
+    @Test
+    void getAll()
+    {
+    }
+
+    @Test
+    void create()
+    {
+    }
+
+    @Test
+    void update()
+    {
+    }
+
+    @Test
+    void delete()
+    {
+    }
+}
