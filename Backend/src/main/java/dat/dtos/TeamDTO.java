448c05449c019518a9b510e4f52783009d2e61b4
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-04-28 12:56:07 +0200

    Add @Data to generate equals and hashCode and toString

--- Backend/src/main/java/dat/dtos/TeamDTO.java
+++ Backend/src/main/java/dat/dtos/TeamDTO.java
@@ -2,13 +2,12 @@ package dat.dtos;
 
 import dat.entities.Team;
 import dat.enums.Game;
-import lombok.*;
+import lombok.Data;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class TeamDTO {
 
     private int id;
