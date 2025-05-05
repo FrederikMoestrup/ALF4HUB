fd013083fb1e2946c526b777ef8e00876e1557a1
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2025-05-05 14:20:28 +0200

    oprettet metoden checkNameExists i TournamentController og metoden nameExists i TournamentDAO til brug af min endpoint /checkname. Derudover har jeg lavet TournamentRouteIntegrationTest.java til test at endpointet

--- Backend/src/main/java/dat/controllers/TournamentController.java
+++ Backend/src/main/java/dat/controllers/TournamentController.java
@@ -8,6 +8,7 @@ import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.util.List;
+import java.util.Map;
 
 public class TournamentController {
 
@@ -91,4 +92,22 @@ public class TournamentController {
                 .check(t -> t.getEndTime() != null && !t.getEndTime().isEmpty(), "End time must be set")
                 .get();
     }
+
+    public void checkNameExists(Context ctx) throws ApiException{
+        String name = ctx.queryParam("name");
+        if (name == null || name.isEmpty()) {
+            throw new ApiException(400, "Missing or invalid parameter: name");
+        }
+
+        boolean exists = tournamentDAO.nameExists(name);
+        String message = exists
+                ? "Tournament name is already taken"
+                : "Tournament name is available";
+
+        ctx.json(Map.of(
+                "exists", exists,
+                "message", message
+        ));
+    }
+
 }
