a68b32d5242b1966f96d87a64825400c79b9917a
Umair-230104 <cph-ut38@cphbusiness.dk>
2025-05-05 13:42:16 +0200

    deleted filer + hvor filerne er brugt

--- Backend/src/main/java/dat/routes/Routes.java
+++ Backend/src/main/java/dat/routes/Routes.java
@@ -10,7 +10,6 @@ public class Routes {
     private final TeamRoute teamRoute = new TeamRoute();
     private final PlayerAccountRoute playerAccountRoute = new PlayerAccountRoute();
     private final TournamentRoute tournamentRoute = new TournamentRoute();
-    private final WordCheckRoute wordCheckRoute = new WordCheckRoute();
 
     public EndpointGroup getRoutes() {
         return () -> {
@@ -18,7 +17,6 @@ public class Routes {
             path("/teams", teamRoute.getRoutes());
             path("/player-accounts", playerAccountRoute.getRoutes());
             path("/tournaments", tournamentRoute.getRoutes());
-            path("/word-checks", wordCheckRoute.getRoutes());
         };
     }
 }
