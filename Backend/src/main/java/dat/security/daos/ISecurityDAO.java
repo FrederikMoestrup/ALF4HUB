f623e4f1770e742a805193d64d00a92bc99f1371
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2025-05-09 21:44:14 +0200

    Remove empty line

--- Backend/src/main/java/dat/security/daos/ISecurityDAO.java
+++ Backend/src/main/java/dat/security/daos/ISecurityDAO.java
@@ -7,6 +7,5 @@ import dat.dtos.UserDTO;
 public interface ISecurityDAO {
     UserDTO getVerifiedUser(String username, String password) throws ValidationException;
     User createUser(String username, String password);
-
     User addRole(int id, String newRole);
 }
