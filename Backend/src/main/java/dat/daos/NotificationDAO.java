9dce7eb0d6c85ead3482b5f8783861f66d21cc33
Umair-230104 <cph-ut38@cphbusiness.dk>
2025-05-12 23:31:06 +0200

    Oprettet Notification.java, NotificationDTO, NotificationDAO + tilføjet en relation til User.java + added Notification i HibernateConfig.java

---
+++ Backend/src/main/java/dat/daos/NotificationDAO.java
@@ -0,0 +1,94 @@
+package dat.daos;
+
+import dat.dtos.NotificationDTO;
+import dat.dtos.TournamentDTO;
+import dat.entities.Notification;
+import dat.entities.Tournament;
+import dat.entities.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import dat.exceptions.ApiException;
+
+import java.util.List;
+
+public class NotificationDAO implements IDAO <NotificationDTO, Integer>
+{
+    private static NotificationDAO instance;
+    private static EntityManagerFactory emf;
+
+    public static NotificationDAO getInstance(EntityManagerFactory _emf)
+    {
+        if (instance == null)
+        {
+            emf = _emf;
+            instance = new NotificationDAO();
+        }
+        return instance;
+    }
+
+    @Override
+    public NotificationDTO getById(Integer id) throws ApiException{
+        try (EntityManager em = emf.createEntityManager()) {
+            Notification notification = em.find(Notification.class, id);
+            if (notification == null) {
+                throw new ApiException(404, "Notification not found");
+            }
+            return new NotificationDTO(notification);
+        }
+    }
+
+    @Override
+    public List<NotificationDTO> getAll(){
+        try (EntityManager em = emf.createEntityManager()) {
+            List<Notification> notifications = em.createQuery("SELECT n FROM Notification n", Notification.class).getResultList();
+            return notifications.stream().map(NotificationDTO::new).toList();
+        }
+    }
+
+    @Override
+    public NotificationDTO create(NotificationDTO notificationDTO)
+    {
+        return null;
+    }
+
+    public NotificationDTO create(NotificationDTO dto, User user) {
+        Notification notification = new Notification(dto, user);
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(notification);
+            em.getTransaction().commit();
+        }
+        return new NotificationDTO(notification);
+    }
+
+    @Override
+    public NotificationDTO update(Integer id, NotificationDTO dto) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Notification notification = em.find(Notification.class, id);
+            if (notification == null) {
+                throw new ApiException(404, "Notification not found");
+            }
+            notification.setRead(dto.isRead());
+            notification.setNotificationTitle(dto.getNotificationTitle());
+            em.getTransaction().commit();
+            return new NotificationDTO(notification);
+        }
+    }
+
+    @Override
+    public NotificationDTO delete(Integer id) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            Notification notification = em.find(Notification.class, id);
+            if (notification == null) {
+                throw new ApiException(404, "Notification not found");
+            }
+            em.remove(notification);
+            em.getTransaction().commit();
+            return new NotificationDTO(notification);
+        }
+    }
+
+}
+
