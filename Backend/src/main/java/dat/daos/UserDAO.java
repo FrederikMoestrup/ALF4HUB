bbc9418a381d55f3f1bc99201e2df5acf71d6973
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2025-05-18 10:46:29 +0200

    Gemmer ændringer i UserDAO.java før branch switch

--- Backend/src/main/java/dat/daos/UserDAO.java
+++ Backend/src/main/java/dat/daos/UserDAO.java
@@ -2,12 +2,13 @@ package dat.daos;
 
 import dat.dtos.UserDTO;
 import dat.entities.User;
+import dat.exceptions.ApiException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.util.List;
 
-public class UserDAO{
+public class UserDAO implements IDAO<UserDTO, Integer> {
 
     private static UserDAO instance;
     private static EntityManagerFactory emf;
@@ -20,7 +21,74 @@ public class UserDAO{
         return instance;
     }
 
-    //Remember SecurityDAO has methods for creating and verifying users and do we really need to delete users?
-    //So we are not implementing IDAO here. But we can use this DAO for other methods.
+    @Override
+    public UserDTO getById(Integer id) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            User user = em.find(User.class, id);
+            if (user == null) {
+                throw new ApiException(404, "User not found");
+            }
+            return new UserDTO(user);
+        }
+    }
 
+    public UserDTO getByUsername(String username) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            User user = em.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+            if (user == null) {
+                throw new ApiException(404, "User not found");
+            }
+            return new UserDTO(user);
+        }
+    }
+
+    public User getUserByUsername(String username) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            User user = em.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+                    .setParameter("username", username)
+                    .getSingleResult();
+            if (user == null) {
+                throw new ApiException(404, "User not found");
+            }
+            return user;
+        }
+    }
+
+    public UserDTO addStrike(Integer id) throws ApiException {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            User user = em.find(User.class, id);
+            if (user == null) {
+                throw new ApiException(404, "User not found");
+            }
+            user.addStrike();
+            em.merge(user);
+            em.getTransaction().commit();
+            return new UserDTO(user);
+        }
+    }
+
+
+    //Husk SecurityDAO har create. Pas på med delete og update. Måske skal getAll bruges?
+    @Override
+    public List<UserDTO> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public UserDTO create(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public UserDTO update(Integer integer, UserDTO userDTO) throws ApiException {
+        return null;
+    }
+
+    @Override
+    public UserDTO delete(Integer integer) throws ApiException {
+        return null;
+    }
 }
