8d32830f8d259b8d0d7b3b089bdb8a1276b7547c
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2025-05-21 18:11:24 +0200

    cucumber template for AK3 i US17

---
+++ Backend/src/test/java/US17/steps/deleteTeamSteps.java
@@ -0,0 +1,32 @@
+package US17.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class deleteTeamSteps {
+
+
+
+    @Given("the team captain confirms the warning message")
+    public void the_team_captain_confirms_the_warning_message() {
+
+    }
+
+    @Then("all players should be removed from the team")
+    public void all_players_should_be_removed_from_the_team() {
+
+    }
+
+    @Then("all associated tournament teams should be removed from the team")
+    public void all_associated_tournament_teams_should_be_removed_from_the_team() {
+
+    }
+
+    @Then("the team should be permanently deleted from the system")
+    public void the_team_should_be_permanently_deleted_from_the_system() {
+
+    }
+
+}
