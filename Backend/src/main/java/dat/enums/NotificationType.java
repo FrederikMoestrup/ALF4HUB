2c28282c60c796e5c69cb9fd7e03f80d3ca4f838
Mahdi Karimi Cph <cph-mk864@cphbusiness.dk>
2025-05-22 19:37:47 +0200

    #141 Vis ikon til notifikationer med badge #142 Skjul badge, hvis ingen nye notifikationer #156 Opret notifikation ved afvisning #157 opret notifikation ved accept af holdansøgning

--- Backend/src/main/java/dat/enums/NotificationType.java
+++ Backend/src/main/java/dat/enums/NotificationType.java
@@ -4,5 +4,5 @@ public enum NotificationType {
     INVITATION,        // Brugeren er inviteret til et hold
     REQUEST,           // Brugeren har sendt en anmodning der er accepteret eller afvist
     INFO,              // Generel information (fx “Velkommen”)
-    MESSAGE            // Beskeder (fx i fremtidig indbakke)
+    MESSAGE,            // Beskeder (fx i fremtidig indbakke)
 }
