b85a1b44034278eae5fc8092a89a9905bc0fa54f
claus0200 <claus.peter.joergensen@gmail.com>
2025-05-22 09:45:21 +0200

    user story 20 acceptance criteria 1

---
+++ Backend/src/test/java/resources/feature/BlogAC1Steps.java
@@ -0,0 +1,22 @@
+package resources.feature;
+
+import io.cucumber.java.en.*;
+
+public class BlogAC1Steps {
+
+
+    @Given("^I see a blog post in the list$")
+    public void iSeeABlogPostInTheList() {
+
+    }
+
+    @When("^i click on the blog post$")
+    public void iClickOnTheBlogPost() {
+
+    }
+
+    @Then("^I should be taken to a page with the full content, author and date$")
+    public void iShouldBeTakenToAPageWithTheFullContentAuthorAndDate() {
+
+    }
+}
