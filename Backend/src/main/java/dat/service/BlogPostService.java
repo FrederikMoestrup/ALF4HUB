b5b6466347ace2ee99f697edf15ad921b361da69
Loudi <loudimail@gmail.com>
2025-05-24 19:48:24 +0200

    fixed draft getting copied and added update method to drafts

--- Backend/src/main/java/dat/service/BlogPostService.java
+++ Backend/src/main/java/dat/service/BlogPostService.java
@@ -26,6 +26,26 @@ public class BlogPostService {
         return blogPostDAO.create(blogPostDTO);
     }
 
+    public BlogPostDTO publishDraft(BlogPostDTO blogPostDTO, long draftId) {
+        if (blogPostDTO == null || blogPostDTO.getTitle() == null || blogPostDTO.getTitle().isBlank() || blogPostDTO.getContent() == null || blogPostDTO.getContent().isBlank()) {
+            throw new IllegalArgumentException("Title and content must not be empty.");
+        }
+
+        if (ProfanityFilter.containsProfanity(blogPostDTO.getTitle()) || ProfanityFilter.containsProfanity(blogPostDTO.getContent())) {
+            throw new IllegalArgumentException("Blog post contains profanity.");
+        }
+
+        return blogPostDAO.publishDraft(blogPostDTO, draftId);
+    }
+
+    public BlogPostDTO updateDraft(BlogPostDTO blogPostDTO, long draftId) {
+        if (blogPostDTO == null || blogPostDTO.getTitle() == null || blogPostDTO.getTitle().isBlank() || blogPostDTO.getContent() == null || blogPostDTO.getContent().isBlank()) {
+            throw new IllegalArgumentException("Title and content must not be empty.");
+        }
+
+        return blogPostDAO.updateDraft(blogPostDTO, draftId);
+    }
+
     public BlogPostDTO createBlogPostDraft(BlogPostDTO blogPostDTO) {
         if (blogPostDTO == null || blogPostDTO.getTitle() == null || blogPostDTO.getTitle().isBlank() || blogPostDTO.getContent() == null || blogPostDTO.getContent().isBlank()) {
             throw new IllegalArgumentException("Title and content must not be empty.");
